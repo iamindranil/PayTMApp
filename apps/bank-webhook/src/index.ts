0b211c571d944c5466b1793c9f154a873fc9aed4
iamindranil <indranilchakraborty692@gmail.com>
2025-02-23 12:06:06 +0530

    first commit

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import db from '@repo/db/client'
+const app=express();
+
+app.post("/hdfcWebhook",(req,res)=>{
+    //zod
+    const paymentInformation={
+        token:req.body.token,
+        userId:req.body.user_identifier,
+        amount:req.body.amount
+    }
+})
