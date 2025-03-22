2b4b73060e6a99630bde6da4e02be9355d396ec8
iamindranil <indranilchakraborty692@gmail.com>
2025-03-22 19:30:02 +0530

    p2pUI_&TnxRowLock_&someChanges_added

--- apps/user-app/app/lib/actions/p2pTransfer.ts
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -27,12 +27,18 @@ export async function p2pTransfer(to:string, amount:number){
     //transfer logic
     try{
         await prisma.$transaction(async(tx)=>{
+            await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(fromUserId)} FOR UPDATE`; //row locking
             //get sender balance
             const fromBalance=await tx.balance.findUnique({
                 where:{
                     userId:Number(fromUserId)
                 }
             })
+            // console.log("above sleep");
+            // await new Promise((resolve)=>{
+            //     setTimeout(resolve,5000);
+            // })
+            // console.log("after sleep"); 
             //check
             if(!fromBalance||fromBalance.amount<amount){
                 throw new Error('Insuficient balance')
@@ -49,16 +55,32 @@ export async function p2pTransfer(to:string, amount:number){
                 }
             })
             //update receiver balance
-            await tx.balance.update({
+            //check whether receiver has balance entry or not
+            await tx.balance.upsert({
                 where:{
                     userId:Number(toUserId)
                 },
-                data:{
+                update:{
                     amount:{
                         increment:amount
                     }
+                },
+                create:{
+                    userId:Number(toUserId),
+                    amount:amount,
+                    locked:0
                 }
             })
+            //create an entry in p2pTransfer table
+            await tx.p2pTransfer.create({
+                data:{
+                    fromUserId:Number(fromUserId),
+                    toUserId:toUserId,
+                    amount:amount,
+                    timestamp:new Date()
+                }
+            })
+
         })
     }catch(err){
         console.error("Transaction failed:", err)
